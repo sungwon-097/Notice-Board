52305176cb61f9dea30bb2f63555840e81a69787
sungwon-097 <csw2757@gmail.com>
2022-11-07 16:19:15 +0900

    #10 - jpa 속성과 설정
    - jpa auditing 기능을 쓰기 위해 JpaConfig 기능 추가
    - db접근과 개발 편의를 위해 각종 부트 프로퍼티 적용
    - TODO: 패스워트를 프로퍼티에 노출시켰다. 처리 필요

---
+++ src/project-board/src/main/java/com/example/projectboard/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.example.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("sungwon"); // TODO :spring security 로 인증 기능을 붙이게 될 때 수정 할 것
+    }
+}
