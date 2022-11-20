59c022d77dff2f59c4ea5f5a58c48a84c3a5f957
sungwon-097 <csw2757@gmail.com>
2022-11-21 00:20:14 +0900

    #19 Security 설정을 모든 요청에 열리도록 수정
    개발단계이므로 permitAll 시키고 로그인 form은 존재하도록 함.

---
+++ src/project-board/src/main/java/com/example/projectboard/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }
+}
